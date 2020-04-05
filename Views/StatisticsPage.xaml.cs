a0dcb03530271dd320af322a556157ec6292639e
noahlehmann <noah.lehmann@protonmail.com>
2020-04-05 19:12:01 +0200

    develop: update view of statistic page

--- Views/StatisticsPage.xaml.cs
+++ Views/StatisticsPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +26,40 @@ namespace ProjectTimeTrackerWPF.Views
         public StatisticsPage()
         {
             InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Chrome",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(8) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Mozilla",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(6) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Opera",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(10) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Explorer",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(4) },
+                    DataLabels = true
+                }
+            };
+
+            DataContext = this;
+
         }
+
+        public SeriesCollection SeriesCollection { get; set; }
+       
     }
 }
